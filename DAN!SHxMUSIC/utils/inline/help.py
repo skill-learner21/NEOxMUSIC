83ee093ec32bbf4f6e45731883504147f38caef9
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:52:43 +0530

    Update help.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/inline/help.py
+++ DAN!SHxMUSIC/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
+from DanishXmusic21 import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
