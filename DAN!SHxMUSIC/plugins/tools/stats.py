b1d7cdb70a36614485ddca1eed25da4204a43b1b
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:44:33 +0530

    Update stats.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/tools/stats.py
+++ DAN!SHxMUSIC/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.userbot import assistants
-from AnonXMusic.misc import SUDOERS, mongodb
-from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from DanishXmusic21 import app
+from DanishXmusic21.core.userbot import assistants
+from DanishXmusic21.misc import SUDOERS, mongodb
+from DanishXmusic21.plugins import ALL_MODULES
+from DanishXmusic21.utils.database import get_served_chats, get_served_users, get_sudoers
+from DanishXmusic21.utils.decorators.language import language, languageCB
+from DanishXmusic21.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
