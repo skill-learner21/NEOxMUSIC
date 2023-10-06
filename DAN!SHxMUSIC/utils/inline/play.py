b0f403449c4bd9240ba8cc014d8e7840cac76bf6
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:53:27 +0530

    Update play.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/inline/play.py
+++ DAN!SHxMUSIC/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from AnonXMusic.utils.formatters import time_to_seconds
+from DanishXmusic21.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
