e5bb79d8e6d84ec0f040d509c729f56e57a8e79a
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:38:08 +0530

    Update active.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/tools/active.py
+++ DAN!SHxMUSIC/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from DanishXmusic21 import app
+from DanishXmusic21.misc import SUDOERS
+from DanishXmusic21.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
