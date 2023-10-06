08cddae8be26831cc22bc0c0c9b457e1e9b6d9ac
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:29:49 +0530

    Update autoend.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/sudo/autoend.py
+++ DAN!SHxMUSIC/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from DanishXmusic21 import app
+from DanishXmusic21.misc import SUDOERS
+from DanishXmusic21.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
