6aae528f86f9f1de846762479b1b9e5c3c486508
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:42:22 +0530

    Update queue.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/tools/queue.py
+++ DAN!SHxMUSIC/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from DanishXmusic21 import app
+from DanishXmusic21.misc import db
+from DanishXmusic21.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from DanishXmusic21.utils.database import get_cmode, is_active_chat, is_music_playing
+from DanishXmusic21.utils.decorators.language import language, languageCB
+from DanishXmusic21.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
