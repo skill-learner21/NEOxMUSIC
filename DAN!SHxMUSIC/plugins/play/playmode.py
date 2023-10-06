6a55fc434f33e8f23cbb482a135974bedccad53a
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:29:09 +0530

    Update playmode.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/play/playmode.py
+++ DAN!SHxMUSIC/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from AnonXMusic.utils.decorators import language
-from AnonXMusic.utils.inline.settings import playmode_users_markup
+from DanishXmusic21 import app
+from DanishXmusic21.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from DanishXmusic21.utils.decorators import language
+from DanishXmusic21.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
