e6be1b93f242fbe2483e49ba406b37506da2d028
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:33:56 +0530

    Update gban.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/sudo/gban.py
+++ DAN!SHxMUSIC/plugins/sudo/gban.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils import get_readable_time
-from AnonXMusic.utils.database import (
+from DanishXmusic21 import app
+from DanishXmusic21.misc import SUDOERS
+from DanishXmusic21.utils import get_readable_time
+from DanishXmusic21.utils.database import (
     add_banned_user,
     get_banned_count,
     get_banned_users,
@@ -15,8 +15,8 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     remove_banned_user,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from DanishXmusic21.utils.decorators.language import language
+from DanishXmusic21.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
