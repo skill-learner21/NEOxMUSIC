543eb7d889d8caef1b5e7545f6a25df61a77a7f2
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:03:44 +0530

    Update stop.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/admins/stop.py
+++ DAN!SHxMUSIC/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from DanishXmusic21 import app
+from DanishXmusic21.core.call import Anony
+from DanishXmusic21.utils.database import set_loop
+from DanishXmusic21.utils.decorators import AdminRightsCheck
+from DanishXmusic21.utils.inline import close_markup
 from config import BANNED_USERS
 
 
