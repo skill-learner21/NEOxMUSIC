4d236d1b92672d65dbf176c72b85f265ec6f1659
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:21:15 +0530

    Update broadcast.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/misc/broadcast.py
+++ DAN!SHxMUSIC/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from DanishXmusic21 import app
+from DanishXmusic21.misc import SUDOERS
+from DanishXmusic21.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from DanishXmusic21.utils.decorators.language import language
+from DanishXmusic21.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
