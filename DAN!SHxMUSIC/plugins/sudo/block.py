b1d16a505ea237f40915815c8621894ffd7d63c2
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:31:56 +0530

    Update block.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/sudo/block.py
+++ DAN!SHxMUSIC/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_gban_user, remove_gban_user
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from DanishXmusic21 import app
+from DanishXmusic21.misc import SUDOERS
+from DanishXmusic21.utils.database import add_gban_user, remove_gban_user
+from DanishXmusic21.utils.decorators.language import language
+from DanishXmusic21.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
