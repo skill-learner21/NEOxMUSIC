33ae87ce9dcc9626bfa90768235b9e3d01a851ec
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 08:58:55 +0530

    Update seek.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/admins/seek.py
+++ DAN!SHxMUSIC/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from DanishXmusic21 import YouTube, app
+from DanishXmusic21.core.call import Anony
+from DanishXmusic21.misc import db
+from DanishXmusic21.utils import AdminRightsCheck, seconds_to_min
+from DanishXmusic21.utils.inline import close_markup
 from config import BANNED_USERS
 
 
