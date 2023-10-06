63d0c1bf6e4b6442f1fa84adc25e9aa94781d928
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-06 09:05:17 +0530

    Update help.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/plugins/bot/help.py
+++ DAN!SHxMUSIC/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
-from AnonXMusic.utils.database import get_lang
-from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from DanishXmusic21 import app
+from DanishXmusic21.utils import help_pannel
+from DanishXmusic21.utils.database import get_lang
+from DanishXmusic21.utils.decorators.language import LanguageStart, languageCB
+from DanishXmusic21.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
