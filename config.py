6622d8730323d99ffd4e8b133252278c98d2446b
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2023-10-17 09:50:52 +0530

    Update config.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/skill-learner21/DanishXmusic21",
+    "https://github.com/skill-learner21/NEOxMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
