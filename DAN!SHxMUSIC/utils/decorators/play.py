66a8dfa936a2f11d7853a4e3c202e9d8185c1989
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2024-04-12 17:28:07 +0530

    Update play.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/decorators/play.py
+++ DAN!SHxMUSIC/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DanishXmusic21 import YouTube, app
-from DanishXmusic21.misc import SUDOERS
-from DanishXmusic21.utils.database import (
+from DAN!SHxMUSIC import YouTube, app
+from DAN!SHxMUSIC.misc import SUDOERS
+from DAN!SHxMUSIC.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from DanishXmusic21.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from AnonXMusic.utils.inline import botplaylist_markup
+from DAN!SHxMUSIC.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
