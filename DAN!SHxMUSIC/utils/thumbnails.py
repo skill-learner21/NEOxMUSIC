203baf5d015920a981621d9e738f825235d028d3
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2024-04-12 17:10:39 +0530

    Update thumbnails.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/thumbnails.py
+++ DAN!SHxMUSIC/utils/thumbnails.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
+from DAN!SHxMUSIC import app
 from config import YOUTUBE_IMG_URL
 
 
