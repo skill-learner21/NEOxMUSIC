44fad4efb4552004a23247c960eb951441cdfe70
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2024-04-12 17:12:32 +0530

    Update logger.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/logger.py
+++ DAN!SHxMUSIC/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import is_on_off
+from DAN!SHxMUSIC import app
+from DAN!SHxMUSIC.utils.database import is_on_off
 from config import LOGGER_ID
 
 
