f6815b4ef2adf5cd4945c924b1348d08f669380d
MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>
2024-04-12 17:18:24 +0530

    Update channelplay.py
    Signed-off-by: MR_DAN!SH21 <98104040+skill-learner21@users.noreply.github.com>

--- DAN!SHxMUSIC/utils/channelplay.py
+++ DAN!SHxMUSIC/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from DAN!SHxMUSIC import app
+from DAN!SHxMUSIC.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
